f624daa26385cac16abb11fa65427e2a2cbc350a
alioutas <alioutas@gmail.com>
2023-04-14 11:23:09 -0400

    revcomp code

---
+++ revcomp.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+# # Make a function that will reverse complement a DNA sequence
+def revcomp(seq):
+    # check if seq is a DNA sequence, must contain only A, T, C, G and N
+    if not set(seq.upper()).issubset(set('ATCGN')):
+        st.error('Not a DNA sequence, must contain only A, T, C, G and N')
+        return
+    seq = seq.upper()
+    seq = seq.replace('A', 't')
+    seq = seq.replace('T', 'a')
+    seq = seq.replace('C', 'g')
+    seq = seq.replace('G', 'c')
+    seq = seq.upper()
+    seq = seq[::-1]
+    return seq
+
+st.title('Reverse Complement DNA Sequence')
+
+# Get the DNA sequence from the user
+seq = st.text_input('Enter your DNA sequence')
+
+# # If the user has entered a sequence, then show the reverse complement
+# keep on showing reverse complements for this session
+
+if seq:
+    st.write('Reverse complement :')
+    st.write(revcomp(seq))
