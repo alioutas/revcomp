a92a236b113bc20cb2dd247eadbfcbbfb8ed9d93
alioutas <alioutas@gmail.com>
2023-04-15 14:09:54 -0400

    select between RNA and DNA

--- revcomp.py
+++ revcomp.py
@@ -8,31 +8,47 @@ footer {visibility: hidden;}
 """
 st.markdown(hide_menu, unsafe_allow_html=True)
 
-# # Make a function that will reverse complement a DNA sequence
-def revcomp(seq):
-    # check if seq is a DNA sequence, must contain only A, T, C, G and N
-    if not set(seq.upper()).issubset(set('ATCGN')):
-        st.error('Not a DNA sequence, must contain only A, T, C, G and N')
-        return
-    seq = seq.upper()
-    seq = seq.replace('A', 't')
-    seq = seq.replace('T', 'a')
-    seq = seq.replace('C', 'g')
-    seq = seq.replace('G', 'c')
-    seq = seq.upper()
-    seq = seq[::-1]
-    return seq
 
-st.title('Reverse Complement DNA Sequence')
-st.markdown('###### by Antonios Lioutas')
+def reverse_complement(sequence, molecule):
+    if molecule == "DNA":
+        complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C', 'N': 'N'}
+    elif molecule == "RNA":
+        complement = {'A': 'U', 'U': 'A', 'C': 'G', 'G': 'C', 'N': 'N'}
+
+    return ''.join([complement[base] for base in reversed(sequence)])
+
+st.title('Reverse Complement DNA/RNA Sequence')
+st.caption('by Antonios Lioutas')
 st.markdown('---')
 
-# Get the DNA sequence from the user
-seq = st.text_input('Enter your DNA sequence')
+input_sequence = st.text_input("Enter your DNA or RNA sequence")
 
-# # If the user has entered a sequence, then show the reverse complement
-# keep on showing reverse complements for this session
+# Determine if input is DNA or RNA
+if all(base in 'ATGCN' for base in input_sequence):
+    input_type = "DNA"
+elif all(base in 'AUGC' for base in input_sequence):
+    input_type = "RNA"
+else:
+    input_type = None
+    st.error("Invalid input. Please enter a valid DNA or RNA sequence.")
 
-if seq:
-    st.write('Reverse complement :')
-    st.write(revcomp(seq))
+if input_type is not None:
+    st.write(f"Input sequence is {input_type}")
+    st.markdown('---')
+
+    # Allow user to choose output type (DNA or RNA)
+    output_type = st.radio("Select output nucleic acid type", ("DNA", "RNA"))
+
+    # Convert input to output type if necessary
+    if input_type != output_type:
+        if output_type == "DNA":
+            conversion = {'U': 'T'}
+        else:
+            conversion = {'T': 'U'}
+        input_sequence = ''.join([conversion.get(base, base) for base in input_sequence])
+
+    # Calculate reverse complement and display it
+    if input_sequence:
+        reverse_complement_sequence = reverse_complement(input_sequence, output_type)
+        st.write(f"Reverse complement {output_type} sequence:")
+        st.subheader(reverse_complement_sequence)
