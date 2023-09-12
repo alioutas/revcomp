52c65d368993ecbd648469c2a8fba523de18deb8
alioutas <alioutas@gmail.com>
2023-09-11 21:14:53 -0400

    buyCoffeeGray

--- revcomp.py
+++ revcomp.py
@@ -59,5 +59,5 @@ if input_type is not None:
 st.markdown('---')
 
 components.html('''
-<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="alioutas" data-color="#40DCA5" data-emoji=""  data-font="Lato" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#ffffff" data-coffee-color="#FFDD00" ></script>
+<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="alioutas" data-color="#808080" data-emoji=""  data-font="Lato" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#ffffff" data-coffee-color="#FFDD00" ></script>
 ''')
